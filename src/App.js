1161b5f05815c9045a1bc2f26dd31cacf145a4f8
Francisco <franc.espinosa6@gmail.com>
2021-04-08 10:19:46 -0400

    created a button and input field

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+    return ( 
+        <div style={{ paddingTop: '10px', marginBlock: '50px' }}>
+            <input type={'text'} placeholder={' Add new task...'}></input>
+            &nbsp; &nbsp;
+            <button>Add</button>
+        </div>
+      );
+}
+
+export default App;
