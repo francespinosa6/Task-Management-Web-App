715c4cfe71f208892269b69a4f30977798d891b4
Francisco <franc.espinosa6@gmail.com>
2021-04-21 11:46:33 -0400

    Added a contact form with React Bootstrap

--- src/contact.js
+++ src/contact.js
@@ -1,11 +1,35 @@
-import react, { useState, useEffect } from 'react';
+import { Form, Button, Row, Col } from 'react-bootstrap';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function Contact(props) {
     return (
-        <div className={'container-fluid'}>
-            contact page
-        </div>
-    );
+        <div className={'container justify-content-center'} style={{ paddingTop: '10px', marginBlock: '50px', textAlign: 'center' }}>
+                <Form>
+                    <Row>
+                        <Col>
+                            <Form.Label><b>First Name: </b></Form.Label>
+                            <Form.Control placeholder="First name" required />
+                        </Col>
+                        <Col>
+                            <Form.Label><b>Last Name:</b></Form.Label>
+                            <Form.Control placeholder="Last name" required />
+                        </Col>
+                    </Row><br/>
+                    <Form.Group controlId="formBasicEmail">
+                        <Form.Label><b>Email address:</b></Form.Label>
+                        <Form.Control type="email" placeholder="name@example.com" required />
+                    </Form.Group>
+                    <Form.Group controlId="exampleForm.ControlTextarea1">
+                        <Form.Label><b>Comments:</b></Form.Label>
+                        <Form.Control as="textarea" rows={3} />
+                    </Form.Group>
+                    <Button variant="primary" type="submit">
+                        Submit
+                    </Button>
+                </Form>
+
+            </div>
+        );
 }
 
 export default Contact;
